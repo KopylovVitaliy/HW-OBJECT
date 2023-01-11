d570f5972cbeea310f5b82fa0c8e2b5190c9bd7b
PioneerX7 <vita-kopy@yandex.ru>
2023-01-11 17:29:19 +0300

    Финальный коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author1 = new Author("Фёдор", "Достоевский");
+        Author author2 = new Author("Александр", "Пушкин");
+
+        Book book1 = new Book("Бесы", author1, 1872);
+        Book book2 = new Book("Евгений Онегин", author2, 1831);
+
+        System.out.println(book1.getTitle() + " " + book1.getBookAuthor() + " " + book1.getYearOfPublication());
+        System.out.println(book2.getTitle() + " " + book2.getBookAuthor() + " " + book2.getYearOfPublication());
+
+        book1.setYearOfPublication(1873);
+        System.out.println("Изменённый год публикации первой книги = " + book1.getYearOfPublication());
     }
 }

319ef2f7ac76b3c9d955f2a298d2e7123cbd21f6
PioneerX7 <vita-kopy@yandex.ru>
2023-01-14 09:03:09 +0300

    Доработка класса Author

--- src/Author.java
+++ src/Author.java
@@ -25,4 +25,17 @@ public class Author {
     public String toString() {
         return firstName + " " + secondName;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return firstName.equals(author.firstName) && secondName.equals(author.secondName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, secondName);
+    }
 }
